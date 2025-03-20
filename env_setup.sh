b0145af858a579ef7a1741722d2cc05fb0435bba
yumeng <yzha5882@uwo.ca>
2025-03-20 10:37:30 -0400

    add ddl

--- env_setup.sh
+++ env_setup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+set -e
+cd "$(dirname "$0")"
 
 ENV_FILE=".env"
 
-sed -i 's|^DATABASE_URL=.*|DATABASE_URL=${DB_URL}|' "$ENV_FILE"
-sed -i 's|^DATABASE_PASSWORD=.*|DATABASE_PASSWORD=${DB_PASSWORD}|' "$ENV_FILE"
+sed -i "s|^DATABASE_URL=.*|DATABASE_URL=${DB_URL}|" $ENV_FILE
+sed -i "s|^DATABASE_PASSWORD=.*|DATABASE_PASSWORD=${DB_PASSWORD}|" $ENV_FILE
